e1b733e5b500406a662812c002c331c2f41b90da
Anton Kovalev <antonko@gmail.com>
2020-02-02 23:26:52 +0700

    Базовые классы запросов

---
+++ RK7Die/CashServer/Query/RK7Cmd.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RK7Die.CashServer.Query
+{
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("XmlSchemaClassGenerator", "2.0.0.0")]
+    [System.Xml.Serialization.XmlTypeAttribute("RK7CMD", Namespace = "", AnonymousType = true)]
+    [System.Xml.Serialization.XmlInclude(typeof(GetOrderList))]
+    public abstract class RK7Cmd
+    {
+        private string cMDField;
+
+        public RK7Cmd()
+        {
+            this.cMDField = "SystemInfo";
+        }
+
+        [System.Xml.Serialization.XmlAttributeAttribute("CMD", Form = System.Xml.Schema.XmlSchemaForm.Unqualified)]
+        public string CMD
+        {
+            get
+            {
+                return this.cMDField;
+            }
+            set
+            {
+                this.cMDField = value;
+            }
+        }
+    }
+}
---
+++ RK7Die/CashServer/Query/RK7Query.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RK7Die.CashServer.Query
+{
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("XmlSchemaClassGenerator", "2.0.0.0")]
+    [System.Xml.Serialization.XmlTypeAttribute("RK7Query", Namespace = "", AnonymousType = true)]
+    [System.Xml.Serialization.XmlRootAttribute("RK7Query", Namespace = "")]
+    public partial class RK7Query
+    {
+        [System.Xml.Serialization.XmlElementAttribute(Form = System.Xml.Schema.XmlSchemaForm.Unqualified)]
+        public RK7Cmd RK7Cmd;
+    }
+}
